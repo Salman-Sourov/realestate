baf0440304c0b5715e82e682907a7c50e1266e90
Salman-Sourov <salman.sourov40@gmail.com>
2023-08-01 04:39:41 +0600

    Backend Template Setup

--- routes/web.php
+++ routes/web.php
@@ -22,9 +22,12 @@ Route::middleware('auth')->group(function () {
 
 require __DIR__.'/auth.php';
 
+///Admin Group Middleware
 Route::middleware(['auth','role:admin'])->group(function(){
 
     Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+    Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
+    Route::get('/admmin/profile', [AdminController::class, 'AdminProfile'])->name('admin.profile');
 
 }); //End Group Admin Middleware
 
@@ -34,3 +37,5 @@ Route::middleware(['auth','role:agent'])->group(function(){
     Route::get('/agent/dashboard', [AgentController::class, 'AgentDashboard'])->name('agent.dashboard');
 
 }); //End Group Agent Middleware
+
+Route::get('/admin/login', [AdminController::class, 'AdminLogin'])->name('admin.login');
