79959e72157f9cf1ea8079911613529dbc8d54f0
Salman-Sourov <salman.sourov40@gmail.com>
2024-08-12 23:45:56 +0600

    Custom Pagination on Frontand

--- resources/views/agent/property/rent_property.blade.php
+++ resources/views/agent/property/rent_property.blade.php
@@ -127,8 +127,6 @@
                         <div class="wrapper list">
                             <div class="deals-list-content list-item">
 
-
-
                                 @foreach ($property as $item)
                                     <div class="deals-block-one">
                                         <div class="inner-box">
@@ -202,19 +200,11 @@
                                     </div>
                                 @endforeach
 
-
-
-
                             </div>
 
                         </div>
                         <div class="pagination-wrapper">
-                            <ul class="pagination clearfix">
-                                <li><a href="property-list.html" class="current">1</a></li>
-                                <li><a href="property-list.html">2</a></li>
-                                <li><a href="property-list.html">3</a></li>
-                                <li><a href="property-list.html"><i class="fas fa-angle-right"></i></a></li>
-                            </ul>
+                            {{ $property->links('vendor.pagination.custom') }}
                         </div>
                     </div>
                 </div>
