5b93a68dd22b186960e11fd41df38d9eebc138f6
Salman-Sourov <salman.sourov40@gmail.com>
2024-09-01 20:55:44 +0600

    Setup Testimonials From Admin Backend

--- resources/views/backend/testimonial/edit_testimonial.blade.php
+++ resources/views/backend/testimonial/edit_testimonial.blade.php
@@ -9,25 +9,29 @@
                 <div class="row">
                     <div class="card">
                         <div class="card-body">
-                            <h6 class="card-title">Add Testimonial </h6>
+                            <h6 class="card-title">Edit Testimonial </h6>
 
-                            <form method="POST" action="{{ route('store.testimonials') }}" class="forms-sample"
+                            <form method="POST" action="{{ route('update.testimonials') }}" class="forms-sample"
                                 enctype="multipart/form-data">
                                 @csrf
 
+                                <input type="hidden" name="id" value="{{ $testimonial->id }}">
+
                                 <div class="mb-3">
                                     <label for="exampleInputEmail1" class="form-label"> Name </label>
-                                    <input type="text" name="name" class="form-control ">
+                                    <input type="text" name="name" class="form-control "
+                                        value="{{ $testimonial->name }}">
                                 </div>
 
                                 <div class="mb-3">
                                     <label for="exampleInputEmail1" class="form-label"> Position </label>
-                                    <input type="text" name="position" class="form-control ">
+                                    <input type="text" name="position" class="form-control "
+                                        value="{{ $testimonial->position }}">
                                 </div>
 
                                 <div class="mb-3">
                                     <label for="exampleInputEmail1" class="form-label"> Message </label>
-                                    <textarea name="message" class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                                    <textarea name="message" class="form-control" id="exampleFormControlTextarea1" rows="3"> {{ $testimonial->message }} </textarea>
                                 </div>
 
                                 <div class="mb-3">
@@ -37,17 +41,17 @@
 
                                 <div class="mb-3">
                                     <label for="exampleInputEmail1" class="form-label"> </label>
-                                    <img id="showImage" class="wd-80 rounded-circle" src="{{ url('upload/no_image.jpg') }}"
+                                    <img id="showImage" class="wd-80 rounded-circle" src="{{ asset($testimonial->image) }}"
                                         alt="profile">
                                 </div>
 
                                 <button type="submit" class="btn btn-primary me-2">Save Changes </button>
                             </form>
+
                         </div>
                     </div>
                 </div>
             </div>
-            <!-- middle wrapper end -->
         </div>
     </div>
 
